6dbf1195bdd3466c50f863307cddf19b40f66ef6
Claudio Carnino <claudio@tugulab.org>
2024-01-28 13:26:10 +0000

    feat: implemented the read length and read string

--- src/database/rdb.rs
+++ src/database/rdb.rs
@@ -58,24 +58,18 @@ impl Database {
         let (pointer, version) = read_headers(pointer)?;
         println!("-> Version: {}", version); // TODO: Store version
 
+        let mut pointer = pointer;
         loop {
             let op_code = RdbOpCode::try_from(pointer[0])?;
-            let _pointer = &pointer[1..];
+            pointer = &pointer[1..];
 
             match op_code {
                 RdbOpCode::EOF => break,
-                // 0x00 => {
-                //     pointer = &pointer[1..];
-                //     let db_number = pointer[0];
-                //     pointer = &pointer[1..];
-                //     println!("-> DB Number: {}", db_number);
-                // }
-                // 0x01 => {
-                //     pointer = &pointer[1..];
-                //     let key = Self::read_string(&mut pointer)?;
-                //     let value = Self::read_string(&mut pointer)?;
-                //     println!("-> Key: {}, Value: {}", key, value);
-                // }
+                RdbOpCode::Aux => {
+                    let (pointer, key) = read_string(&pointer)?;
+                    let (pointer, value) = read_string(&pointer)?;
+                    println!("-> Key: {}, Value: {}", key, value);
+                }
                 _ => anyhow::bail!("-> Unknown op code: {:?}", op_code),
             }
         }
@@ -88,17 +82,45 @@ impl Database {
  * @return (pointer, version)
  */
 fn read_headers(bytes: &[u8]) -> anyhow::Result<(&[u8], u32)> {
-    let pointer = bytes;
-
-    let magic_string = &pointer[..5];
+    let magic_string = &bytes[..5];
     anyhow::ensure!(magic_string == b"REDIS");
-    let pointer = &pointer[5..];
+    let bytes = &bytes[5..];
 
-    let version = String::from_utf8_lossy(&pointer[..4]).to_string();
+    let version = String::from_utf8_lossy(&bytes[..4]).to_string();
     let version = version.parse::<u32>()?;
-    let pointer = &pointer[4..];
+    let bytes = &bytes[4..];
 
-    Ok((pointer, version))
+    Ok((bytes, version))
+}
+
+/**
+ * @return (pointer, length)
+ */
+fn read_length(bytes: &[u8]) -> anyhow::Result<(&[u8], usize)> {
+    let mask = 3u8 << 6; // 1100 0000
+    let (length, bytes_count) = match bytes[0] & mask {
+        0 => (u32::from_be_bytes([0, 0, 0, bytes[0]]), 1),
+        64u8 => (
+            u32::from_be_bytes([0, 0, bytes[0] & (64u8 - 1), bytes[1]]),
+            2,
+        ),
+        128u8 => (u32::from_be_bytes(bytes[1..5].try_into().unwrap()), 5),
+        _ => anyhow::bail!("-> Length encoding not supported"),
+    };
+
+    let bytes = &bytes[bytes_count..];
+    Ok((bytes, length as usize))
+}
+
+/**
+ * @return (pointer, string)
+ */
+fn read_string(bytes: &[u8]) -> anyhow::Result<(&[u8], String)> {
+    let (bytes, length) = read_length(bytes)?;
+    let string = String::from_utf8_lossy(&bytes[..length]).to_string();
+
+    let bytes = &bytes[length..];
+    Ok((bytes, string))
 }
 
 #[cfg(test)]
